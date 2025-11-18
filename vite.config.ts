8746ad574975e8e2ff8b4aa30ab6c0efe8fe6fe0
Hyukjun Kang <patrick322@naver.com>
2025-11-18 12:13:58 +0900

    fix: allowed host에 render 설정

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,11 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: parseInt(process.env.PORT || "5173"),
+    allowedHosts: ["i-made-it.onrender.com"],
   },
   preview: {
     host: "0.0.0.0",
     port: parseInt(process.env.PORT || "4173"),
+    allowedHosts: ["i-made-it.onrender.com"],
   },
 });
