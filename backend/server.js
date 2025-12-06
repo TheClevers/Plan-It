858a2721a7c06ab31f1db87ee1e3b7926cf42a91
kyun0563 <129490377+kyun0563@users.noreply.github.com>
2025-12-06 15:57:52 +0900

    feat: Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://plan-it-dkbz.onrender.com"
+    "https://plan-it-origin-backend.onrender.com"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
 }));
